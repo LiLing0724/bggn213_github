b35fff70884f698acd64574e18d14633623e684d
Li Ling <l3ling@ucsd.edu>
2025-12-05 14:40:47 -0800

    Add some class files

---
+++ Class04/class04/plot_practice.R
@@ -0,0 +1,12 @@
+#' ---
+#' title: "Class 4 Lab"
+#' author: "Li Ling (A15092789)"
+#' ---
+
+#this is my first R script
+x <- 1:50 
+plot(x)
+plot(x,col="pink")
+plot(x, sin(x),typ="l",col="orange", lwd="3")
+log(10, base=10)
+log(10)
